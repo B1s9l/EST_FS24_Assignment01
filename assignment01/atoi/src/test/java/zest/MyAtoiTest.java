e911574fac38dcc9008e616e7474ffb9f18fadb5
Paul TC Huang <paul891112@hotmail.com>
2024-03-14 17:16:34 +0100

    Add coverage and mutation implementation

--- assignment01/atoi/src/test/java/zest/MyAtoiTest.java
+++ assignment01/atoi/src/test/java/zest/MyAtoiTest.java
@@ -1,49 +1,70 @@
 package zest;
 
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 class MyAtoiTest {
 
 
     @Test
     public void convert_string_to_int (){
-        assert MyAtoi.myAtoi("42") == 42;
+        Assertions.assertEquals(MyAtoi.myAtoi("42"), 42);
+        //asser MyAtoi.myAtoi("42") , 42;
     }
 
     @Test
     public void remove_whitespace_keep_negative(){
-        assert MyAtoi.myAtoi(" -42") == -42;
+        Assertions.assertEquals(MyAtoi.myAtoi(" -42"), -42);
     }
 
     @Test
     public void cut_off_trailing_alphabet() {
-        assert MyAtoi.myAtoi("4193 with words") == 4193;
+        Assertions.assertEquals(MyAtoi.myAtoi("4193 with words") , 4193);
     }
 
     @Test
     public void assert_abc_equals_zero(){
-        assert MyAtoi.myAtoi("abc") == 0;
+        Assertions.assertEquals(MyAtoi.myAtoi("abc") , 0);
     }
 
     @Test
     public void input_null(){
-        assert MyAtoi.myAtoi(null) == 0;
+        Assertions.assertEquals(MyAtoi.myAtoi(null) , 0);
     }
 
     @Test
     public void assert_0032_equals_32(){
-        assert MyAtoi.myAtoi("0032") == 32;
+        Assertions.assertEquals(MyAtoi.myAtoi("0032") , 32);
     }
 
     @Test
     public void assert_input_out_of_range(){
-        assert MyAtoi.myAtoi("4294967296") == 2147483647;
+        Assertions.assertEquals(MyAtoi.myAtoi("4294967296") , 2147483647);
     }
 
     @Test
     public void assert_rest_of_string_ignored(){
-        assert MyAtoi.myAtoi("12abc3") == 12;
+        Assertions.assertEquals(MyAtoi.myAtoi("12abc3") , 12);
     }
- 
+
+    @Test
+    public void assert_if_str_length_0(){
+        Assertions.assertEquals(MyAtoi.myAtoi(" ") , 0);
+    }
+    @Test
+    public void assert_input_out_of_lower_bound(){
+        Assertions.assertEquals(MyAtoi.myAtoi("-5019284736") , -2147483648);
+    }
+
+    @Test
+    public void assert_plus_sign(){
+        Assertions.assertEquals(MyAtoi.myAtoi("+12") , 12);
+    }
+
+    @Test
+    public void assert_conditional_boundary_overflow(){
+        Assertions.assertEquals(MyAtoi.myAtoi("2147483646"), 2147483646);
+    }
+
 }
--- assignment01/frac2dec/src/test/java/zest/Frac2DecTest.java
+++ assignment01/frac2dec/src/test/java/zest/Frac2DecTest.java
@@ -2,35 +2,54 @@ package zest;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 class Frac2DecTest {
 
   @Test
   public void assert_one_over_two(){
-      assert Frac2Dec.fractionToDecimal(1,2).equals("0.5");
+      Assertions.assertEquals(Frac2Dec.fractionToDecimal(1,2), "0.5");
  }
 
  @Test
  public void assert_two_over_one(){
-   assert Frac2Dec.fractionToDecimal(2,1).equals("2");
+   Assertions.assertEquals(Frac2Dec.fractionToDecimal(2,1), "2");
  }
 
  @Test
  public void assert_four_over_333(){
-   assert Frac2Dec.fractionToDecimal(4,333).equals("0.(012)");
+   Assertions.assertEquals(Frac2Dec.fractionToDecimal(4,333),"0.(012)");
  }
 
  @Test
  public void assert_zero_denominator(){
-      assert Frac2Dec.fractionToDecimal(5, 0) == null;
+     Assertions.assertNull(Frac2Dec.fractionToDecimal(5, 0));
  }
 
  @Test
  public void assert_first_digits_then_repeat(){
-      System.out.println(Frac2Dec.fractionToDecimal(1401,700));
-      assert  Frac2Dec.fractionToDecimal(1401,700).equals("2.00(142857)");
+      Assertions.assertEquals( Frac2Dec.fractionToDecimal(1401,700),"2.00(142857)");
  }
 
+ @Test
+ public void assert_minus_one_over_two(){
+      Assertions.assertEquals(Frac2Dec.fractionToDecimal(-1, 2), "-0.5");
+ }
+
+ @Test
+    public void assert_one_over_minus_two(){
+      Assertions.assertEquals(Frac2Dec.fractionToDecimal(1, -2), "-0.5");
+ }
+
+ @Test
+    public void assert_minus_one_over_minus_four(){
+      Assertions.assertEquals(Frac2Dec.fractionToDecimal(-1,-4), "0.25");
+ }
+
+ @Test
+ public void assert_nominator_zero(){
+      Assertions.assertEquals(Frac2Dec.fractionToDecimal(0,2), "0");
+ }
 
 
 }
